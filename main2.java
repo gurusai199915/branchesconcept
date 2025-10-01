c52b52d5047f320115d2c6abc82aed499179ae11
gurusai199915 <shankarkalamsetty@gmail.com>
2025-10-01 15:49:53 +0530

    Update main2.java

--- main2.java
+++ main2.java
@@ -2,6 +2,7 @@ public class demo1
   {
  public class void main (String[]args)
     {
-      system.out.println("main method")
-        }
+            system.out.println("main method");
+      system.out.println("main ");
+    
   }
